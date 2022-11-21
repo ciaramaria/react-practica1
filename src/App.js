6ea1dbaf94eeb95cccbf9d5328287050e1411c70
ciaramaria <ciaramariaroder@gmail.com>
2022-11-21 19:56:57 -0300

    react-practica1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import GeneralWrapper from "./components/GeneralWrapper/GeneralWrapper";
+import SmallContainer from "./components/SmallContainer/SmallContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GeneralWrapper>
+      <SmallContainer>
+      <h1>lea</h1>
+      </SmallContainer>
+    </GeneralWrapper>
+
+
   );
 }
 
